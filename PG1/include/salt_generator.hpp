7f74d3ad111e9a30eb9b3e886938948f2de8983f
stephen-pinto <stephen_pinto@outlook.com>
2022-06-26 00:02:40 +0530

    Update salt_generator.hpp

--- PG1/include/salt_generator.hpp
+++ PG1/include/salt_generator.hpp
@@ -20,14 +20,14 @@ std::string cmd_tool::salt_generator::generate_salt(size_t len_in_bytes)
         "0123456789"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz";
-    std::string tmp_s;
-    tmp_s.reserve(len_in_bytes);
 
-    for (size_t i = 0; i < len_in_bytes; ++i) {
-        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
-    }
+    std::string str;
+    str.reserve(len_in_bytes);
+
+    for (size_t i = 0; i < len_in_bytes; ++i) 
+        str += alphanum[rand() % (sizeof(alphanum) - 1)];
     
-    return tmp_s;
+    return str;
 }
 
 #endif // __SALT_GENERATOR_H__
